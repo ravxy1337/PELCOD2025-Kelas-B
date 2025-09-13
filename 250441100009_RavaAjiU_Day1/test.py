22927469af6b01ecac15e58594b942ed9568c508
ravxy1337 <ravaganss83@gmail.com>
2025-09-13 11:43:27 +0700

    250441100009_RavaAjiU_Day1

---
+++ 250441100009_RavaAjiU_Day1/test.py
@@ -0,0 +1,49 @@
+#TEST 1
+nama = "Rava"
+nim = 250441100009
+semester = 1
+
+print("====BIODATA MAHASISWA=====")
+print("halo perkenalkan nama saya: " + nama)
+print(f"Nim saya: : {nim}")
+print("saya saat ini semester: {}".format(semester))
+
+
+#PERHITUNGAN TEST SENDIRI PAK MHEHEHEHE
+angka1 = int(input("masukkan angka kesatu pak: "))
+angka2 = int(input("masukkan angka kedua pak: "))
+
+hasil_plus = angka1 + angka2
+hasil_kurang = angka1 - angka2
+hasil_kali = angka1 * angka2
+hasil_bagi = angka1 / angka2
+hasil_sb = angka1 % angka2
+
+
+print("hasil dari plus: " + str(hasil_plus))
+print("hasil dari kurang: " + str(hasil_kurang))
+print("hasil dari kali: " + str(hasil_kali))
+print("hasil dari bagi: " + str(hasil_bagi))
+print("hasil dari sb: " + str(hasil_sb))
+
+#PERHITUNGAN
+a = 10
+b = 5
+c = a + b
+d = a - b
+e = a * b
+f = a // b
+g = a % b
+
+print("hasil dari penjumlahan: " + str(c))
+
+
+nama = input("masukkan nama anda: ")
+umur = input("masukkan umur anda: ")
+buah = input("masukkan buat favorit: ")
+
+print("nama saya adalah " + nama)
+print("umur saya adalah " + umur)
+print("buat favorit saya adalah " + buah)
+
+
